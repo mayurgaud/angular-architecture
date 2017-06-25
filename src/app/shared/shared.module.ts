d0d5213ebb894ac96843b62313dc43e7ac69aad0
Mayur Gaud <mayur_gaud99@yahoo.com>
2017-06-25 09:10:55 +0200

    First commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        HttpModule
+    ],
+    declarations: []
+})
+export class SharedModule {
+}
